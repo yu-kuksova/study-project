c2b57559192412932722dd54ec152cf875b46dd9
yu-kuksova <kuksova813@gmail.com>
2022-05-18 12:06:49 +0300

    start project

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const cssmin = require('gulp-cssmin')
+
+gulp.task('sass', function(){
+  return gulp.src('app/scss/style.scss')  //указываем с чем что-то делать 
+    .pipe(sass({outputStyle: 'compressed'})) //выполнение + плагин
+    .pipe(rename({suffix: '.min'})) // переименовать файл
+    .pipe(autoprefixer({
+        overrideBrowserslist: ['last 8 versions']
+    }))
+    .pipe(gulp.dest('app/css'))      //куда залить результат
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('html', function(){
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function(){
+    return gulp.src('app/js/*.js')
+       .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+});
+
+gulp.task('watch', function(){
+    gulp.watch('app/scss/style.scss', gulp.parallel('sass'))  
+    gulp.watch('app/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('js'))
+});
+
+
+gulp.task('script', function(){
+    return gulp.src([
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
+})
+
+
+gulp.task('style', function(){
+    return gulp.src([
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/magnific-popup/dist/magnific-popup.css'
+    ])
+        .pipe(concat('libs.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('app/css'))
+})
+
+gulp.task('default', gulp.parallel('sass', 'watch', 'browser-sync', 'script', 'style'))
