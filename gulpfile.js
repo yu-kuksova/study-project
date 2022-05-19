b161556bb83130c5fda1b69dd5c80c391ae68b5d
yu-kuksova <kuksova813@gmail.com>
2022-05-19 12:16:50 +0300

    added products

--- gulpfile.js
+++ gulpfile.js
@@ -46,7 +46,9 @@ gulp.task('watch', function(){
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -58,7 +60,8 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
