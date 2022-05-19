190e0e3273685823fdeaf89650cadc35f2f9c08b
yu-kuksova <kuksova813@gmail.com>
2022-05-19 13:16:01 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 $(function(){
 
-    
     var mixer = mixitup('.products__inner-box');
+
+    $('.rote-stars').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.product__slider-inner').slick({
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+    });
 
 });
