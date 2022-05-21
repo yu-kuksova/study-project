ef04413cf89bc34449db090489625ae233d6a0b9
yu-kuksova <kuksova813@gmail.com>
2022-05-21 23:39:44 +0300

    added new-items-list

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,24 @@
 $(function(){
 
+    
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
+
+
     $('.rote-stars').rateYo({
         rating: 5,
         starWidth: "12px",
         readOnly: true
     });
-    
+
     $(".js-range-slider").ionRangeSlider({
         type: "double",
         min: 0,
@@ -22,6 +35,8 @@ $(function(){
         starWidth: "12px",
         readOnly: true
     });
+
+    
 
     $('.product__slider-inner').slick({
         dots: true,
