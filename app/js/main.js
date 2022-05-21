ed08dc7719ff6e41f1ab0feb3e9b985e3ea31b99
yu-kuksova <kuksova813@gmail.com>
2022-05-21 20:28:56 +0300

    added new-itwms

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
+    $('.rote-stars').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+    
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
     var mixer = mixitup('.products__inner-box');
 
     $('.rote-stars').rateYo({
@@ -14,5 +29,7 @@ $(function(){
         slidesToScroll: 4,
         arrows: false,
     });
+
+    
 
 });
