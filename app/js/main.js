c1270a424fceef8cb1c7fba1ab243ed9ef866ce6
yu-kuksova <kuksova813@gmail.com>
2022-05-24 13:34:53 +0300

    added product-aside

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $(function(){
 
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
+
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
     });
@@ -8,7 +17,6 @@ $(function(){
         $('.header__box').toggleClass('active');
     });
     
-
     
     $('.icon-th-list').on('click', function(){
         $('.product__item').addClass('list');
